18d8fc1147e53615d1b83f7b5803ceff07c5ec11
BrucePBon <marcpm1994@gmail.com>
2020-06-23 17:28:10 +0200

    23062020.1

--- src/LIBTIFF.jl
+++ src/LIBTIFF.jl
@@ -1,299 +1,218 @@
 module LIBTIFF
 
-# package code goes here
-using ColorTypes, FixedPointNumbers
+import Base: eltype, size, getindex, summary
+export TIFFFile, tiffopen, tiffclose, tiffsize, tiffread
+# using FixedPointNumbers, ColorTypes
 
-import Base:
-    eltype, size, getindex, summary
+""" NOTES FOR ME IN THE FUTURE
+ > I am missing error handling, aka err != 1 && ( nsamples[1] = 1 )  in samples_per_pixel 
+ > if bits is not divisible by 2, julia will automatically give an error in getType
+ > not so suitable if i1::Int and i2::Int, no tile fits between first(i2)-1 and last(i2)
+ > the images in /test_data/ are not well labeled and some are not even openable by fiji
+ > a tiff file can contain multiple images of different sizes if the tif file contains multi-resolution views of the same image. I didn't consider this case. 
+""" 
 
-export
-    # types
-    TIFFFile,
-    # functions
-    tiffopen, tiffclose, tiffsize, tiffread
+abstract type Tile     end
+abstract type Scanline end
 
-type_map = Dict(
-    (1, 8) => UInt8,
-    (1, 16) => UInt16,
-    (1, 32) => UInt32,
-    (1, 64) => UInt64,
-    (2, 8) => Int8,
-    (2, 16) => Int16,
-    (2, 32) => Int32,
-    (2, 64) => Int64,
-    (3, 16) => Float16,
-    (3, 32) => Float32,
-    (3, 64) => Float64)
+struct TIFFFile{T <: Union{Tile,Scanline},N}
+    fptr::Ptr{Cvoid}
+	dims::NTuple{N,Int}
+end 
 
-abstract type TIFFDataFormat end
 
-type Tile <: TIFFDataFormat end
-type Scanline <: TIFFDataFormat end
+# Handy variables ------------------------------------
 
-struct TIFFFile{T <: TIFFDataFormat}
-    filehandle::Ptr{Void}
+TIFFField  = (:TIFFGetField,"libtiff");
+Int_Range  = Union{UnitRange{Int},Int}; 
+
+TYPES      = ( UInt8 , Int8,      NaN, 
+               UInt16, Int16, Float16, 
+               UInt32, Int32, Float32,
+               UInt64, Int64, Float64 
+			 );
+
+exceptions = Dict( 
+			   Cstring      => String,
+               Ptr{Nothing} => Union{Array,Ptr{Cvoid}}
+             );
+
+
+""" METAPROGRAMMING TO BRING LIBTIFF FUNCTIONS TO JULIA """
+
+# TIFFGetField acts in-place, overwriting an input array with the Field's value(s). The input must be an array because some TIFFFields return 2 or 3 values, not only 1. Because it is in-place, a variables needs to be created before each call to TIFFGetField, which results in long code. Metaprogramming is used to automate the definition of out-of-place functions for each relevant TIFFTag. 
+tag_data = ( ( "tiff_length"         , UInt32 ,   1   ,  Cuint   , 257 ), 
+			 ( "tiff_width"          , UInt32 ,   1   ,  Cuint   , 256 ), 
+			 ( "tiff_tile_length"    , UInt32 ,   1   ,  Cuint   , 323 ), 
+			 ( "tiff_tile_width"     , UInt32 ,   1   ,  Cuint   , 322 ), 
+			 ( "tiff_sampleformat"   , UInt16 ,   1   ,  Cushort , 339 ), 
+			 ( "tiff_nbits"          , UInt16 ,   1   ,  Cushort , 258 ), 
+			 ( "tiff_samplesperpixel", UInt16 ,   1   ,  Cushort , 277 ) )
+
+for ( p, r, a, w, n ) in tag_data 
+    f1 = """
+    function $p( h::Ptr{Cvoid} )
+	    in = ones( $r, $a );
+	    er = ccall($TIFFField, Cint, (Ptr{Cvoid},$w,Ptr{Cvoid}), h, $n, in);
+		( er == 0 ) && throw("Error retrieving $(p[6:end]) from tiff file")
+	    return ( $a == 1 ) ? round(Int64,in[1]) : round.(Int64,in)
+    end """
+	f2 = "$p(f::TIFFFile) = $p( f.fptr )"
+	eval( Meta.parse( f1 ) ); 
+	eval( Meta.parse( f2 ) );
 end
 
-# opening and closing
+# metaprogramming is used to create julia functions for relevant libtiff functions, so I don't need to write "ccall( ... )" and to memorize the signature of each function. 
+tifffuns = (
+("TIFFOpen"                 , Ptr{Cvoid}, (Cstring,Cstring)                               ),
+("TIFFClose"                , Cint      , (Ptr{Cvoid},)                                   ), 
+("TIFFReadDirectory"        , Cint      , (Ptr{Cvoid},)                                   ), 
+("TIFFSetDirectory"         , Cint      , (Ptr{Cvoid},Cushort)                            ),
+("TIFFIsTiled"              , Cint      , (Ptr{Cvoid},)                                   ), 
+("TIFFReadScanline"         , Cint      , (Ptr{Cvoid},Ptr{Cvoid},Cint,Cint)               ),
+("TIFFReadRGBAImageOriented", Cint      , (Ptr{Cvoid},Clong,Clong,Ptr{Cvoid},Cshort,Cint) ), 
+("TIFFReadTile"             , Csize_t   , (Ptr{Cvoid},Ptr{Cvoid},Cint,Cint,Cint,Cint)     ) ) 
+
+CtoJL( x::DataType ) = ( x in keys(exceptions) ) ? exceptions[x] : x ; 
+
+for ( n, o, i ) in tifffuns
+    args, vars = "", ""; 
+    [ args = args*" a"*string(x)*"::"*string(CtoJL(i[x]))*"," for x in 1:length(i) ]; 
+    [ vars = vars*" a"*string(x)*"," for x in 1:length(i) ]; 
+    f = """
+    $n( $(args[1:end-1]...) ) = ccall( (:$n,"libtiff"), $o, $i, $(vars[1:end-1]...) ); 
+    """
+    println( f ) # uncomment to see what the automated function code looks like
+    eval( Meta.parse( f ) )
+end
+
+
+""" OPENING and CLOSING TIFF FILES """
 
 function tiffopen(filename::String, mode::String="r")
-    fp = ccall((:TIFFOpen, "libtiff"), Ptr{Void}, (Cstring, Cstring), filename, mode)
-    if tiffistiled(fp)
-        tiff = TIFFFile{Tile}(fp)
-    else
-        tiff = TIFFFile{Scanline}(fp)
-    end
-    return tiff
+	isfile(filename) || throw("tif file doesn't exist")
+	h    = TIFFOpen(filename, mode)
+	d, N = tiffdims(h); 
+    return tiffistiled(h) ? TIFFFile{Tile,N}(h,d) : TIFFFile{Scanline,N}(h,d);
 end
 
-function tiffopen(f::Function, args...)
-    file = tiffopen(args...)
-    try
-        f(file)
-    finally
-        tiffclose(file)
-    end
+function tiffopen(fun::Function, filename::String, mode::String)
+ 
+    file = tiffopen(filename, mode)
+    try fun(file) finally; tiffclose(file) end
 end
 
-function tiffclose(tifffile::TIFFFile)
-    ccall((:TIFFClose, "libtiff"), Cint, (Ptr{Void},), tifffile.filehandle)
+function tiffclose(f::TIFFFile)
+	TIFFClose( f.fptr )	
+end 
+
+
+""" UTILITY FUNCTIONS """
+
+# 3D datasets are stored as stacks, where multiple images (directives) are combined in the same tiff file. This function counts the number of directives in a tiff file. All images in a stack should have the same size. After determing the number of images, the directory "pointer" is set back to 0, the first image.
+function tiffdims(fp::Ptr{Cvoid}) 
+	ndir = 1
+	w, h = tiff_width(fp), tiff_length(fp);
+	while Bool( TIFFReadDirectory( fp ) ) 
+		ndir += 1
+		wdir, hdir = tiff_width(fp), tiff_length(fp);
+		wdir == w && hdir == h || throw("Stack of images of different sizes."); 
+	end
+	TIFFSetDirectory( fp, Cushort(0) ) 
+	return ( ndir > 1 ) ? (( h, w, ndir ), 3) : (( h, w ), 2)
 end
 
-# helper functions
+tiffistiled(fp::Ptr{Cvoid}) = Bool( TIFFIsTiled(fp) )
 
-function tiffgetfield!(value, tag, file::TIFFFile)
-    err = ccall((:TIFFGetField, "libtiff"), Cint, (Ptr{Void}, Clong, Ptr{Void}), file.filehandle, tag, value)
-    return err
-end
+      getType(format, bits) = TYPES[ Int(log(2,bits)-3)*3 + format ];
 
-function tiffistiled(fp::Ptr{Void})
-    is_tiled = ccall((:TIFFIsTiled, "libtiff"), Cint, (Ptr{Void},), fp)
-    return Bool(is_tiled)
-end
+  tifftilesize(f::TIFFFile) = Int.([tiff_tile_length(f),tiff_tile_width(f)])   
 
-function tiffsize(filename::String)
-    shape = tiffopen(filename) do file
-        tiffsize(file)
-    end
-    return shape
-end
+       summary(f::TIFFFile) = "$(size(f)[1])x$(size(f)[2])$(typeof(f))" 
 
-function tiffsize(file::TIFFFile)
-    tmp_length = Array{UInt32}(1)
-    tmp_width = Array{UInt32}(1)
-    tiffgetfield!(tmp_length, 257, file)
-    tiffgetfield!(tmp_width, 256, file)
-    shape = Int[Int(tmp_length[1]), Int(tmp_width[1])]
-    return shape
-end
-
-size(file::TIFFFile) = tiffsize(file)
-
-function tifftilesize(file::TIFFFile{Tile})
-    length = Array{UInt32}(1)
-    width = Array{UInt32}(1)
-    tiffgetfield!(length, 323, file)
-    tiffgetfield!(width, 322, file)
-    tiles = Int[Int(length[1]), Int(width[1])]
-    return tiles
-end
-
-function tifftilesize(filename::String)
-    ts = tiffopen(filename) do file
-        tifftilesize(file)
-    end
-    return ts
-end
-
-function sample_format(file::TIFFFile)
-    sf = fill(UInt16(1), 1)
-    tiffgetfield!(sf, 339, file)
-    return Int(sf[1])
-end
-
-function sample_format(filename::String)
-    sf = tiffopen(filename) do file
-        sample_format(file)
-    end
-    return sf
-end
-
-function n_bits(file::TIFFFile)
-    sf = fill(UInt16(0), 1)
-    tiffgetfield!(sf, 258, file)
-    return Int(sf[1])
-end
-
-function n_bits(filename::String)
-    sf = tiffopen(filename) do file
-        n_bits(file)
-    end
-    return sf
-end
-
-function tiffdtype(filename::String)
-    dt = tiffopen(filename) do file
-        tiffdtype(file)
-    end
-    return dt
-end
-
-function tiffdtype(file::TIFFFile)
-    sampleformat = sample_format(file)
-    nbits = n_bits(file)
-    try
-        dt = type_map[sampleformat, nbits]
-        return dt
-    catch
-        println(sampleformat, nbits)
-    end
-    return NaN
-end
-
-eltype(file::TIFFFile) = tiffdtype(file)
-
-function samples_per_pixel(file::TIFFFile)
-    nsamples = Array{Int16}(1)
-    err = tiffgetfield!(nsamples, 277, file)
-    if err != 1
-        nsamples[1] = 1
-    end
-
-    return Int(nsamples[1])
-end
-
-function summary{T}(file::TIFFFile{T})
-    s = size(file)
-    str = "$(s[1])x$(s[2]) TIFFFile{$T}"
-    return str
-end
-
-# reading
-# tiled tiffs
-
-function read_tile(yrange, xrange, file::TIFFFile{Tile}, ::Type{Val{N}}) where {N}
-    nsamples = samples_per_pixel(file)
-    info = tile_info(file, yrange, xrange)
-    TYPE = eltype(file)
-    large = nsamples, (info["endy"] - info["starty"]) * info["tiley"], (info["endx"] - info["startx"]) * info["tilex"]
-    data = Array{TYPE}(large...)
-
-    buffer = Array{TYPE}(nsamples, info["tiley"], info["tilex"])
-    for y=info["starty"]:info["tiley"]:info["endy"]
-        for x=info["startx"]:info["tilex"]:info["endx"]
-            ccall((:TIFFReadTile, "libtiff"), Csize_t, (Ptr{Void}, Ptr{Void}, Cint, Cint, Cint, Cint), file.filehandle, buffer, x, y, 0, 0)
-            ex = x + info["tilex"]
-            ey = y + info["tiley"]
-            data[:, y+1:ey, x+1:ex] = permutedims(buffer, [1, 3, 2])
-        end
-    end
-
-    arr_buf = data[:, yrange[1]-info["offsety"]:yrange[2]-info["offsety"] - 1, xrange[1]-info["offsetx"]:xrange[2]-info["offsetx"] - 1]
-    return arr_buf
-end
-
-function read_tile(yrange, xrange, file::TIFFFile{Tile}, ::Type{Val{1}})
-    info = tile_info(file, yrange, xrange)
-    TYPE = eltype(file)
-    large = (info["endy"] - info["starty"]) * info["tiley"], (info["endx"] - info["startx"]) * info["tilex"]
-    data = Array{TYPE}(large...)
-
-    buffer = Array{TYPE}(info["tiley"], info["tilex"])
-    for y=info["starty"]:info["tiley"]:info["endy"]
-        for x=info["startx"]:info["tilex"]:info["endx"]
-            ccall((:TIFFReadTile, "libtiff"), Csize_t, (Ptr{Void}, Ptr{Void}, Cint, Cint, Cint, Cint), file.filehandle, buffer, x, y, 0, 0)
-            ex = x + info["tilex"]
-            ey = y + info["tiley"]
-            data[y+1:ey, x+1:ex] = buffer'
-        end
-    end
-
-    arr_buf = data[yrange[1]-info["offsety"]:yrange[2]-info["offsety"] - 1, xrange[1]-info["offsetx"]:xrange[2]-info["offsetx"] - 1]
-    return arr_buf
-end
-
-function tile_info(file::TIFFFile{Tile}, yrange, xrange)
-    tiles = tifftilesize(file)
-    startx = xrange[1] ÷ tiles[2]
-    starty = yrange[1] ÷ tiles[1]
-    endx = xrange[2]
-    endy = yrange[2]
-    offsetx = startx * tiles[2] - 1
-    offsety = starty * tiles[1] - 1
-    info = Dict("tilex" => tiles[2], "tiley" => tiles[1],
-                "startx" => startx, "starty" => starty,
-                "endx" => endx, "endy" => endy,
-                "offsetx" => offsetx, "offsety" => offsety)
-    return info
+function eltype(f::TIFFFile) # work around, one of my stacks was stored in UInt, which is sampleformat code 1, but got an error. Defaulting to Cushort(1); 
+	 sf = Cushort(1); 
+	 try
+		 sf = tiff_sampleformat(f); 
+	 catch
+	 end
+	 return getType( sf, tiff_nbits(f)) 
 end
 
 
-function tiffread(file::TIFFFile{Tile}, indices::Vararg{Union{UnitRange{Int}, Int}, 2})
-    nsamples = samples_per_pixel(file)
-    s = size(file)
-    yrange = first(indices[1]) - 1, last(indices[1])
-    xrange = first(indices[2]) - 1, last(indices[2])
-    if yrange[2] > s[1] || xrange[2] > s[2]
-        throw(BoundsError(file, indices))#"$(s[1])x$(s[2]) TIFFFile at index [$(indices[1]), $(indices[2])]"))
-    end
-    return read_tile(yrange, xrange, file, Val{nsamples})
+
+""" READING SCANLINE TIFFs """
+
+function tiffread(f::TIFFFile{Scanline,N}) where {N}
+	return ( tiff_samplesperpixel(f) > 1 ) ? read_rgba_scanline(f) : read_scanline(f);  
 end
 
-function tiffread(file::TIFFFile{Tile})
-    s = size(file)
-    return tiffread(file, 1:s[1], 1:s[2])
+function read_scanline(f::TIFFFile{Scanline,N}) where {N}
+	data = zeros( eltype(f),    f.dims    )
+    buff = zeros( eltype(f), 1, f.dims[2] )
+	maxZ = ( N == 2 ) ? 0 : f.dims[3] - 1; 
+	for z = 0:maxZ
+		TIFFSetDirectory( f.fptr, Cushort(z) ) 
+		for r = 1:f.dims[1]
+			TIFFReadScanline( f.fptr, buff, Cint(r-1), Cint(0) )
+		    data[r, :, z+1] = buff'	
+		end
+	end
+    return data
 end
 
-# scanline tiffs
-function read_scanline!(data::Array{T}, file::TIFFFile{Scanline}) where {T}
-    buffer = Array{T}(size(data, 2))
-    for row = 1:size(data, 1)
-        ccall((:TIFFReadScanline, "libtiff"), Cint, (Ptr{Void}, Ptr{Void}, Cint, Cint), file.filehandle, buffer, row-1, 0)
-        data[row, :] = buffer
-    end
-    return nothing
-end
-
-function read_rgba_scanline(file::TIFFFile{Scanline})
-    shape = size(file)
-    buffer = Array{UInt32}(shape[2], shape[1]) # transposed because libtiff saves row major
-    ccall((:TIFFReadRGBAImageOriented, "libtiff"), Cint, (Ptr{Void}, Clong, Clong, Ptr{Void}, Cshort, Cint), file.filehandle, shape[2], shape[1], buffer, 1, 0)
+function read_rgba_scanline(f::TIFFFile{Scanline,2}) 
+    buffer = Array{UInt32}(f.dims[2], f.dims[1]) # transposed because libtiff saves row major
+	TIFFReadRGBAImageOriented( f.fptre, f.dims[2], f.dims[1], 1, 0 )
     buffer = buffer'
-    n0f8_data = unsafe_wrap(Array, Ptr{UInt8}(pointer(buffer)), (4, shape[1], shape[2]))
+    n0f8_data = unsafe_wrap(Array, Ptr{UInt8}(pointer(buffer)), (4, f.dims[1], f.dims[2]))
     return n0f8_data
 end
 
-function tiffread(file::TIFFFile{Scanline})
-    TYPE = eltype(file)
-    nsamples = samples_per_pixel(file)
-    if nsamples > 1
-        return read_rgba_scanline(file)
-    else
-        data = Array{TYPE}(size(file)...)
-        read_scanline!(data, file)
-        return data
-    end
+
+""" READING TILED TIFFs """
+# Images are divided into squares, tiles, of size n*16. Each tile has coordinates (x,y) where x and y are between 0:imgWidth÷tileWidth and 0:imgHeight÷tileHeight. Generally, one can restrict image loading to a rectangle by providing (minCol, maxCol, minRow, maxRow) and setting x and y between minCol÷tileWidth:maxCol÷tileWidth and minRow÷tileWidth:maxRow÷tileWidth
+
+function tiffread(f::TIFFFile{Tile,2})
+	return read_tile(f, 1:f.dims[1], 1:f.dims[2])
 end
 
-# general interface for reading
-function tiffread(filename::String, indices::Vararg{Union{UnitRange{Int}, Int}, 2})
-    arr = tiffopen(filename) do file
-        tiffread(file, indices...)
-    end
-    return arr
+function read_tile(f::TIFFFile{Tile,2}, i1::Int_Range, i2::Int_Range ) 
+    last(i1) <= f.dims[1] && last(i2) <= f.dims[2] || throw("BoundError in read_tile") 
+    return read_tile( first(i2)-1, last(i2), first(i1)-1, last(i1), f )
 end
+
+# should work for 3 channels (RGB) and 1 channel. Can't test it. I need test images that work. 
+function read_tile(minW, maxW, minH, maxH, f::TIFFFile{Tile}) 
+	
+	tileH,tileW = tifftilesize(f); # offY,offX=startY*tileW-1,startX*tileH-1; 
+	println( tileH, " ", tileW )
+	tileHcoords = minH÷tileH:maxH÷tileW; 
+	tileWcoords = minW÷tileW:maxW÷tileW; 
+	ntilesH     = length( tileHcoords );
+	ntilesW     = length( tileWcoords ); 
+	nchannels   = tiff_samplesperpixel(f); 
+	data = zeros( eltype(f), nchannels, tileW*ntilesW, tileH*ntilesH )
+	buff = zeros( eltype(f), nchannels, tileW, tileH )
+
+    for row in tileHcoords, col in tileWcoords
+		println( row, " ", col, " ", tileH, "  ", tileW ) 
+		TIFFReadTile(f.fptr, buff, Cint(col), Cint(row), Cint(0), Cint(0)); 
+        data[:, col+1:col+tileW, row+1:row+tileH] = permutedims(buff, [1, 3, 2]);
+    end
+    return data[:, minW+1:maxW, minH+1:maxH ]
+end
+
+
+""" GENERAL READING INTERFACE """ 
 
 function tiffread(filename::String)
-    arr = tiffopen(filename) do file
-        tiffread(file)
-    end
-    return arr
+	f = tiffopen(filename)
+	data = tiffread(f)
+	tiffclose(f)
+    return data
 end
 
-function tiffread(file::TIFFFile, indices::Vararg{Union{UnitRange{Int}, Int}, 2})
-    data = tiffread(file)
-    return data[indices...]
-end
 
 function getindex(file::TIFFFile, indices::Vararg{Union{UnitRange{Int}, Int}, 2})
     return tiffread(file, indices...)
